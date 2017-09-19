8c4018be3196be97f7d50491f70939f7ba55ef71
Taylor Tard <trolltaylor145@gmail.com>
2017-09-18 22:39:39 -0500

    Added basic tusk abilites they do nothing

--- game/frostivus_comp/scripts/vscripts/addon_game_mode.lua
+++ game/frostivus_comp/scripts/vscripts/addon_game_mode.lua
@@ -28,7 +28,6 @@ end
 -- Evaluate the state of the game
 function CAddonTemplateGameMode:OnThink()
 	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
-		--print( "Template addon script is running." )
 	elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
 		return nil
 	end
